ebe6300b06c92900fea9f09c781e7474f4c34aa3
Andres Munevar <afmunevarm@hotmail.com>
2021-02-06 02:24:21 -0500

    Create controller and router for Group Bills,  no functions defined yet

---
+++ api/controllers/groupbills/groupbills.controller.js
@@ -0,0 +1,106 @@
+const db = require('../../../sequelize');
+const GroupBill = db.GroupBills;
+
+class GroupBillController {
+    createGroupBill(req, res) {
+
+        if (!req.body.name) {
+            res.status(400).send({
+                message: "Please enter the name of the sample."
+            });
+            return;
+        }
+
+        const sample = {
+            sid: req.body.sid,
+            name: req.body.name
+        };
+
+        Sample.create(sample)
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || "An error occurred while creating the sample."
+                });
+            });
+    }
+
+    findOne(req, res) {
+        const id = req.params.id;
+
+        Sample.findByPk(id)
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: "Unfortunately we were unable to retrieve this sample."
+                });
+            });
+    }
+
+    findAll(req, res) {
+        Sample.findAll()
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || 'Unfortunately we were unable to retrieve all samples.'
+                });
+            });
+    }
+
+    update(req, res) {
+        const id = req.params.id;
+
+        Sample.update(req.body, {
+                where: { catid: id }
+            })
+            .then(num => {
+                if (num == 1) {
+                    res.send({
+                        message: "The sample has been updated successfully"
+                    });
+                } else {
+                    res.send({
+                        message: "Unfortunately this sample could not be found, please double check the ID."
+                    });
+                }
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: "Unable to delete this sample."
+                })
+            })
+    }
+
+    delete(req, res) {
+        const id = req.params.id;
+
+        Sample.destroy({
+                where: { catid: id }
+            })
+            .then(num => {
+                if (num == 1) {
+                    res.send({
+                        message: "The sample was deleted successfully!"
+                    });
+                } else {
+                    res.send({
+                        message: "Unfortunately this sample could not be found, please double check the ID."
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(id)
+                res.status(500).send({
+                    message: "Unable to delete this sample."
+                })
+            })
+    }
+}
+
+module.exports = GroupBillController;
---
+++ routes/groupbills.route.js
@@ -0,0 +1,19 @@
+const GroupBillController = require('../api/controllers/groupbills/groupbills.controller');
+const controller = new GroupBillController();
+
+module.exports = (app) => {
+
+    let router = require("express").Router();
+
+    router.post("/", controller.createGroupBill);
+
+    router.get("/:id", controller.findOne)
+
+    router.get("/", controller.findAll)
+
+    router.put("/:id", controller.update)
+
+    router.delete("/:id", controller.delete)
+
+    app.use('/api/samples', router)
+}
