52bb6f0f5968494c8fd0f09bc1dbd9889ea8bfb8
thisisomar <abusamro@sheridancollege.ca>
2021-02-06 20:15:23 -0500

    Google Cloud Deployment

--- config/config.js
+++ config/config.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
+require('dotenv').config();
 
 module.exports = {
   development: {
+    dialect: "postgres",
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync('./certs/cc-ca.crt').toString()
+      }
+    }
+  },
+  production: {
     dialect: "postgres",
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
